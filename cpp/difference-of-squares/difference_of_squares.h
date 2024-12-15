8a5871ef1ec2660f5718af1824f518e04489d4e0
Mike Rivnak <rivnakm1@gmail.com>
2024-12-14 22:05:21 -0500

    cpp: difference-of-squares

---
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,11 @@
+#if !defined(DIFFERENCE_OF_SQUARES_H)
+#define DIFFERENCE_OF_SQUARES_H
+
+namespace difference_of_squares
+{
+int square_of_sum(int num);
+int sum_of_squares(int num);
+int difference(int num);
+} // namespace difference_of_squares
+
+#endif // DIFFERENCE_OF_SQUARES_H
